66cb84cac708dab539caae7ae05e28f18e34e466
ajkarimi7 <ajkarimi7@gmail.com>
2019-04-30 18:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function happyHolidays(string) {
-  console.log("Happy holidays!")
-  return `Happy holidays!`
-}
-
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
-}
-
-function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
-
-function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!` 
-}
+function happyHolidays()
